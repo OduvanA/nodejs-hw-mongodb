cf25a208438aced488429c948c61fdd82531b967
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-09-22 20:59:54 -0500

    hw05 sign in almost done

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from "./utils/env.js";
-import contactsRouter from './routers/contacts.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
+import router from './routers/index.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', 3000));
 
@@ -18,8 +19,9 @@ export const setupServer = () => {
     },
   }),);
   app.use(express.json());
+  app.use(cookieParser());
 
-  app.use(contactsRouter);
+  app.use(router);
 
   app.use('*', notFoundHandler);
 
